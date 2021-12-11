21d4074c11e99cd1ca5f48c85e64c307cf915936
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2021-12-11 17:01:13 +0100

    Refactor day 11

--- 2021/11/main.kt
+++ 2021/11/main.kt
@@ -2,41 +2,22 @@ import java.io.File
 
 data class Octopus(var value: Int, val x: Int, val y: Int, var flashed: Boolean = false)
 
-fun List<List<Octopus>>.increaseEach() = this.map { it.map { Octopus(it.value + 1, it.x, it.y) } }
+fun List<List<Octopus>>.energizeThem() = this.flatten().forEach { it.value++ }
 
-fun List<List<Octopus>>.flashThemAll() = this.map { row -> 
-    row.map { octopus ->
-        if (octopus.value > 9) {
-            octopus.flash(this)
-        }
-        octopus
-    } 
-}
-
-fun List<List<Octopus>>.step3() = this.map { row ->
-    row.map { octopus -> 
-        if (octopus.flashed) {
-            Octopus(0, octopus.x, octopus.y)
-        } else {
-            Octopus(octopus.value, octopus.x, octopus.y)
-        }
-    }
-}
+fun List<List<Octopus>>.flashThem() = this.flatten().filter { it.value > 9 }.forEach { it.flash(this) }
 
 fun Octopus.flash(octopuses: List<List<Octopus>>) {
     if (this.flashed) return
 
-    val neighbors = this.neighbors(octopuses)
-    neighbors.forEach { 
-        it.value += 1
-    }
     this.flashed = true
-    neighbors.forEach {
+
+    this.getNeighbors(octopuses).forEach { 
+        it.value += 1
         if (it.value > 9) it.flash(octopuses)
     }
 }
 
-fun Octopus.neighbors(octopuses: List<List<Octopus>>) = 
+fun Octopus.getNeighbors(octopuses: List<List<Octopus>>) = 
     (this.y - 1..this.y + 1)
         .filter { it >= 0 && it < octopuses.size }
         .map { row -> 
@@ -47,6 +28,13 @@ fun Octopus.neighbors(octopuses: List<List<Octopus>>) =
         }
         .flatten()
 
+fun List<List<Octopus>>.countThem() = this.flatten().count { it.flashed }
+
+fun List<List<Octopus>>.resetThem() = this.flatten().filter { it.flashed }.forEach { 
+    it.value = 0
+    it.flashed = false
+}
+
 
 fun main() {
     val octopuses = File("input.txt")
@@ -54,20 +42,27 @@ fun main() {
         .map { it.chunked(1).map { it.toInt() } }
         .mapIndexed { y, row -> row.mapIndexed { x, value -> Octopus(value, x, y) } }
     
-    println((1..100).fold(Pair(0, octopuses)) { current, _ -> 
-        val (flashes, currentOctopuses) = current
-        val afterFlashing = currentOctopuses.increaseEach().flashThemAll()
-        val flashCount = afterFlashing.flatten().count { it.flashed }
-        val stepResult = afterFlashing.step3()
-        Pair(flashes + flashCount, stepResult)
-    }.first)
+    println((1..100).fold(0) { numOfFlashes, _ -> 
+        octopuses.energizeThem()
+        octopuses.flashThem()
+        val flashCount = octopuses.countThem()
+        octopuses.resetThem()
+        numOfFlashes + flashCount
+    })
 
-    println((1..1000).fold(Triple(0, -1, octopuses)) { current, step -> 
-        val (flashes, firstFlash, currentOctopuses) = current
-        val afterFlashing = currentOctopuses.increaseEach().flashThemAll()
-        val flashCount = afterFlashing.flatten().count { it.flashed }
-        val stepResult = afterFlashing.step3()
-        val newFirstFlash = if (flashCount == 100 && firstFlash == -1) step else firstFlash
-        Triple(flashes + flashCount, newFirstFlash, stepResult)
-    }.second)
+    val octopuses2 = File("input.txt")
+        .useLines { it.toList() }
+        .map { it.chunked(1).map { it.toInt() } }
+        .mapIndexed { y, row -> row.mapIndexed { x, value -> Octopus(value, x, y) } }
+
+    var step = 0
+    while (true) {
+        step++
+        octopuses2.energizeThem()
+        octopuses2.flashThem()
+        if (octopuses2.countThem() == 100) break
+        octopuses2.resetThem()
+    }
+
+    println(step)
 }
